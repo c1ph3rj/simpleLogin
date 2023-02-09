9c33ba0f977536b3cde2c5982109ebe06d5639a2
c1ph3rj <jeevarag1016@gmail.com>
2023-02-10 01:39:06 +0530

    f

--- app/src/main/java/com/c1ph3rj/simplelogin/LoginScreen.kt
+++ app/src/main/java/com/c1ph3rj/simplelogin/LoginScreen.kt
@@ -3,6 +3,7 @@ package com.c1ph3rj.simplelogin
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.content.res.AppCompatResources
 import com.c1ph3rj.simplelogin.databinding.ActivityLoginScreenBinding
 
@@ -11,17 +12,15 @@ class LoginScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_screen)
-
+        viewBindLogin = ActivityLoginScreenBinding.inflate(layoutInflater)
+        setContentView(viewBindLogin.root)
         init()
     }
 
     private fun init(){
         try{
-            val loginPicView = viewBindLogin.loginPicView
             val listOfLoginPic = listOf(R.drawable.login_pic_one, R.drawable.login_pic_two, R.drawable.login_pic_three, R.drawable.login_pic_four)
 
-            loginPicView.setImageDrawable(AppCompatResources.getDrawable(this, listOfLoginPic.random()))
         }catch (e: Exception){
             e.printStackTrace()
         }

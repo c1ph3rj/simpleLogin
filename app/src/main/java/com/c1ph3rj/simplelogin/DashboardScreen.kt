14d5a11c1bfb13c9ac45487fdcd8c022437b7bf8
Jeeva Prakash <jeevarag1016@gmail.com>
2023-02-14 13:56:04 +0530

    commit

--- app/src/main/java/com/c1ph3rj/simplelogin/DashboardScreen.kt
+++ app/src/main/java/com/c1ph3rj/simplelogin/DashboardScreen.kt
@@ -1,20 +1,26 @@
 package com.c1ph3rj.simplelogin
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.view.View.OnTouchListener
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.CompositePageTransformer
 import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
 import com.c1ph3rj.simplelogin.databinding.ActivityDashboardScreenBinding
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.math.abs
+
 
 class DashboardScreen : AppCompatActivity() {
-    private final val ApiKey : String = "17e09070dd3c40efa6739f5e23d57aa3"
-    private lateinit var viewBindDashBoard : ActivityDashboardScreenBinding
+    private final val ApiKey: String = "17e09070dd3c40efa6739f5e23d57aa3"
+    private lateinit var viewBindDashBoard: ActivityDashboardScreenBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +30,8 @@ class DashboardScreen : AppCompatActivity() {
         init()
     }
 
-    private fun init(){
-        try{
+    private fun init() {
+        try {
             val topHeadlinesView = viewBindDashBoard.topHeadlinesView
 
             Thread {
@@ -39,41 +45,65 @@ class DashboardScreen : AppCompatActivity() {
 
                     apiCall.getTopTechNews("technology", "en", ApiKey)
                         .enqueue(object : Callback<NEWSResponse?> {
+                            @SuppressLint("ClickableViewAccessibility")
                             override fun onResponse(
                                 call: Call<NEWSResponse?>,
                                 response: Response<NEWSResponse?>
                             ) {
                                 println(response)
                                 if (response.isSuccessful) {
+                                    val indicatorScrollView = viewBindDashBoard.tabIndicatorScroll
+                                    indicatorScrollView.setOnTouchListener { _, _ ->
+                                        true
+                                    }
                                     val responseFromApi: NEWSResponse? = response.body()
                                     if (responseFromApi != null) {
-                                        val headlinesViewAdapter = topHeadlinesViewAdapter(this@DashboardScreen, responseFromApi.articles)
-                                        val tabDotsForHeadlinesView = viewBindDashBoard.headlinesViewIndicator
+                                        val headlinesViewAdapter = topHeadlinesViewAdapter(
+                                            this@DashboardScreen,
+                                            responseFromApi.articles
+                                        )
+                                        val tabDotsForHeadlinesView =
+                                            viewBindDashBoard.headlinesViewIndicator
+
                                         topHeadlinesView.adapter = headlinesViewAdapter
-                                        topHeadlinesView.setClipToPadding(false)
-                                        topHeadlinesView.setClipChildren(false)
-                                        topHeadlinesView.setOffscreenPageLimit(3)
-                                        topHeadlinesView.getChildAt(0)
-                                            .setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER)
 
-                                        // Code for carousel view animation in viewpager2.
-
-                                        // Code for carousel view animation in viewpager2.
-                                        val compositePageTransformer = CompositePageTransformer()
-                                        compositePageTransformer.addTransformer(
-                                            MarginPageTransformer(40)
-                                        )
-                                        compositePageTransformer.addTransformer { page: View, position: Float ->
-                                            val `val` = 1 - Math.abs(position)
-                                            page.scaleY = 0.95f + `val` * 0.15f
-                                        }
-
-                                        topHeadlinesView.setPageTransformer(
-                                            compositePageTransformer
-                                        )
                                         TabLayoutMediator(
                                             tabDotsForHeadlinesView, topHeadlinesView
-                                        ) { tab: TabLayout.Tab?, position: Int -> }.attach()
+                                        ) { _: TabLayout.Tab?, _: Int -> }.attach()
+
+                                        topHeadlinesView.registerOnPageChangeCallback(object :
+                                            ViewPager2.OnPageChangeCallback() {
+                                            override fun onPageScrolled(
+                                                position: Int,
+                                                positionOffset: Float,
+                                                positionOffsetPixels: Int
+                                            ) {
+                                                super.onPageScrolled(
+                                                    position,
+                                                    positionOffset,
+                                                    positionOffsetPixels
+                                                )
+
+                                                if(!topHeadlinesView.isFakeDragging){
+                                                    val scrollPosition = position - 5
+                                                    if(scrollPosition >= 0){
+                                                        indicatorScrollView.scrollX =
+                                                            tabDotsForHeadlinesView.getTabAt(scrollPosition)!!.view.x.toInt()
+                                                    }
+
+
+                                                    if(scrollPosition > 0){
+                                                        tabDotsForHeadlinesView.getTabAt(scrollPosition)!!.view.scaleX = -0.5F
+                                                        tabDotsForHeadlinesView.getTabAt(scrollPosition)!!.view.scaleY = -0.5F
+                                                    }
+
+                                                    println(scrollPosition)
+                                                }
+
+
+
+                                            }
+                                        })
 
                                     }
                                 }
@@ -87,7 +117,7 @@ class DashboardScreen : AppCompatActivity() {
                     e.printStackTrace()
                 }
             }.start()
-        }catch(e: Exception){
+        } catch (e: Exception) {
             e.printStackTrace()
         }
 
